d5594d172661b033b4ac85d8430cb64dad69223f
Anna Smets <anna.smets@student,ehb.be>
2025-02-20 20:25:12 +0100

    slotmachine working only gotta reset

--- js/index.js
+++ js/index.js
@@ -1,27 +1,48 @@
-import {
-    slotMachine
-} from "./slotMachine.js";
+import { slotMachine } from "./slotMachine.js";
+let result = document.getElementById("result");
 
 function init() {
-    console.log("Init");
-    // TODO: log the document using console.log to test if you can get access to it
-    // TODO: log the submit button. If that works, store that button in a variable
-    // TODO: attach an event listener to that button and show a log "button clicked"
-    // TODO: if the submit button is clicked, the lever is pulled (= call that function)
+	console.log("Init");
+	// TODO: log the document using console.log to test if you can get access to it
+	// TODO: log the submit button. If that works, store that button in a variable
+	// TODO: attach an event listener to that button and show a log "button clicked"
+	// TODO: if the submit button is clicked, the lever is pulled (= call that function)
+	console.log(document);
+	console.log(document.getElementById("button"));
+	let button = document.getElementById("button");
+	button.addEventListener("click", function () {
+		console.log("button clicked");
+		pullLever();
+	});
 }
 
 function pullLever() {
-    // TODO: reset the machine (you may skip this step for now, and focus on getting the machine to work first)
-    // TODO: spin the slot machine
-    // TODO: show slots and win status
+	// TODO: reset the machine (you may skip this step for now, and focus on getting the machine to work first)
+	// TODO: spin the slot machine
+	// TODO: show slots and win status
+	slotMachine.reset();
+	showSlots();
+	showGameResult();
 }
 
 function showSlots() {
-    // TODO: show the slot symbols in HTML
+	// TODO: show the slot symbols in HTML
+	//result.innerHTML = slotMachine.spin();
+	slotMachine.spin().forEach((element) => {
+		result.innerHTML += element;
+	});
 }
 
 function showGameResult() {
-    // TODO: show a win or lose message in HTML
+	// TODO: show a win or lose message in HTML
+	let status = document.getElementById("status");
+	slotMachine.calculateStatus();
+	if (slotMachine.win === true) {
+		status.innerHTML = "you win";
+	} else {
+		status.innerHTML = "you lose";
+	}
 }
 
 init();
+showSlots();
--- js/slotMachine.js
+++ js/slotMachine.js
@@ -1,18 +1,38 @@
 export const slotMachine = {
-    calculateStatus: function () {
-        // TODO: check for three similar symbols and update 'win'-boolean
-    },
-    getRandomSymbol: function () {
-        // TODO: return a single random symbol
-    },
-    reset: function () {
-        // TODO: Empty out the slots and reset 'win'-boolean
-    },
-    symbols: ["♠", "♥", "♣", "♦"],
-    slots: [],
-    slotsAmount: 3,
-    spin: function () {
-        // TODO: generate three random symbols, and add them to the slots of the slotMachine
-    },
-    win: true
+	calculateStatus: function () {
+		// TODO: check for three similar symbols and update 'win'-boolean
+		if (
+			this.slots.at(0) === this.slots.at(1) &&
+			this.slots.at(0) === this.slots.at(2)
+		) {
+			this.win = true;
+		} else {
+			this.win = false;
+		}
+	},
+	getRandomSymbol: function (symbols) {
+		// TODO: return a single random symbol
+		function getRndNumb(min, max) {
+			return Math.floor(Math.random() * (max - min + 1)) + min;
+		}
+		let rndNumb = getRndNumb(0, 3);
+		return symbols.at(rndNumb);
+	},
+	reset: function () {
+		// TODO: Empty out the slots and reset 'win'-boolean
+		this.slots = [];
+		this.win = true;
+	},
+	symbols: ["♠", "♥", "♣", "♦"],
+	slots: [],
+	slotsAmount: 3,
+	spin: function () {
+		// TODO: generate three random symbols, and add them to the slots of the slotMachine
+		for (let i = 0; i <= this.slotsAmount - 1; i++) {
+			this.slots.push(this.getRandomSymbol(this.symbols));
+			console.log(this.slots);
+		}
+		return this.slots;
+	},
+	win: true,
 };
